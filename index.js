c1ac2f6ed97e92a75ede1a9571ccdfac82508e52
Lionel Manassé <lionelmutombo.mtb@gmail.com>
2024-04-07 12:39:30 +0100

    resolution des algo

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 // 1. Écrivez une fonction qui prend un nombre positif "n" en paramètre et retourne une chaîne de caractères contenant des nombres de 1 à "n"
 
 function displayNumbers(n) {
-    return 0
-
+    let char = '';
+    if (n === 0) {
+        return '0';
+    } else {
+        for (let i = 1; i <= n; i++) {
+            char += `${i} `;           
+        }
+        return char.trim();
+    }
 }
-
+displayNumbers(10);
 // 2. Écrivez une fonction qui prend un nombre "n" en paramètre et qui retourne une chaîne de caractère qui contient tous les nombres impairs de 1 à "n".
 
 function displayOddNumbers(n) {
-    return 0
+    let char = '';
+    if (n === 0) {
+        return '0';
+    } else {
+        for (let i = 1; i <= n; i++) {
+            if (i % 2 !== 0) {
+                char += `${i} `;
+            }            
+        }
+        return char.trim();
+    }
 }
+displayOddNumbers(10);
 
 
 // 3. Écrivez une fonction qui retourne une chaîne de caractère qui contient toutes les tables de multiplication de 1 à 10.
@@ -35,26 +53,46 @@ function displayOddNumbers(n) {
  */
 
 function displayAllMultiplicationTables() {
-    return 0
-
+    let table = '';
+    for (let i = 1; i <= 10; i++) {
+        for (let j = 1; j <= 10; j++) {
+            table += `${i} * ${j} = ${i * j}\n`;
+        }    
+        table += `\n`    
+    }
+    return table.trim();
 }
-
+displayAllMultiplicationTables();
 // 4. Écrivez une fonction qui prend deux nombres en paramètre n et m et qui retourne la somme de tous les nombres entre n et m.
 //
 // calcule la somme de tous les nombres entre 1 et 100.
 
 function sumAllNumbers(n, m) {
-    return 0
-
+    let sum = 0;
+    if (m < n) {
+        return '0';
+    } else {
+        for (let i = n; i <= m; i++) {
+            sum += i;    
+        }
+        return sum;
+    }
 }
-
+sumAllNumbers(1, 0);
 // 5. Écrivez une fonction qui retourne la moyenne de tous les nombres entre 1 et 100.
 
 function averageAllNumbers() {
-    return 0
+    let sum = 0;
+    let moyenne;
+    let count;
 
+    for (let i = 1; i <= 100; i++) {
+        sum += i;
+        count = i;            
+    }
+    return moyenne = sum / count;
 }
-
+averageAllNumbers();
 
 
 module.exports = {
